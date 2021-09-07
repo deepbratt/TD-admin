35c1aa7c1af6a15be9cb3771eb1e490de5c5772d
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2021-09-07 16:45:44 +0500

    added font and updated header

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -1,49 +1,59 @@
 import {
   AppBar,
+  Typography,
   IconButton,
   makeStyles,
   Toolbar,
   Drawer,
 } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+// import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import { useState } from "react";
 import SideBar from "../SiderBar";
+
+const drawerWidth = 240;
 
 const HeaderStyles = makeStyles((theme) => ({
   toolbar: {
     display: "flex",
     justifyContent: "space-between",
   },
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
   drawerPaper: {
+    width: drawerWidth,
     backgroundColor: theme.palette.secondary.main,
   },
 }));
 
 const Header = () => {
-  const { toolbar, drawerPaper } = HeaderStyles();
-  const [open, setOpen] = useState(true);
-
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+  const { appBar, toolbar, drawer, drawerPaper } = HeaderStyles();
 
   return (
-    <AppBar color="inherit">
+    <AppBar color="inherit" className={appBar}>
       <Toolbar className={toolbar}>
-        <IconButton edge="start" color="inherit" onClick={toggleDrawer}>
-          <MenuIcon />
-        </IconButton>
+        <Typography variant="h6" noWrap>
+          Tez Dealz Admin Panel
+        </Typography>
         <IconButton color="inherit">
           <AccountCircle />
         </IconButton>
       </Toolbar>
       <Drawer
+        className={drawer}
+        variant="permanent"
         classes={{
           paper: drawerPaper,
         }}
-        open={open}
-        onClose={toggleDrawer}
       >
         <SideBar />
       </Drawer>
