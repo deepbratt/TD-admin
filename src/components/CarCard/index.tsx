8cdb9911d6eff45a3e931f6516f4324a0fb9646e
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-09-09 16:51:11 +0500

    code refactor

--- src/components/CarCard/index.tsx
+++ src/components/CarCard/index.tsx
@@ -18,11 +18,9 @@ import CardStyles from "./styles";
 import NoImg from "../../assets/icons/no-img.png";
 import { Colors } from "../../theme/themeConstants";
 import moment from "moment";
-import { useState } from "react";
-import { API_ENDPOINTS } from "../../utils/API/endpoints";
-import { deleteData, updateData } from "../../utils/API/APIs";
 import Toast from "../Toast";
 import Loader from "../Loader";
+import useCarCard from "./useCarCard";
 
 interface CarCardProps {
   data: any;
@@ -31,72 +29,18 @@ interface CarCardProps {
 
 const CarCard = ({ data, layoutType = "list" }: CarCardProps) => {
   const { root, grid, featuredBadge, location } = CardStyles();
-  const [isBanned, setIsBanned] = useState(data.banned);
-  const [isActive, setIsActive] = useState(data.active);
-  const [isLoading, setIsLoading] = useState(false);
-  const [toastOpen, setToastOpen] = useState(false);
-  const [toastMessage, setToastMessage] = useState("");
-  const [toastType, setToastType] = useState("success");
-
-  const toggleBan = () => {
-    let banUnban = isBanned ? API_ENDPOINTS.MARK_UNBAN : API_ENDPOINTS.MARK_BAN;
-    setIsLoading(true);
-    updateData(
-      `${API_ENDPOINTS.ADS}${API_ENDPOINTS.CARS}${banUnban}/${data._id}`
-    ).then((response: any) => {
-      if (response && response.data && response.data.status === "success") {
-        setIsBanned(!isBanned);
-        setToastMessage(response.data.message);
-        setToastType("success");
-      } else {
-        console.log(response);
-        setToastMessage(response.message);
-        setToastType("error");
-      }
-      setToastOpen(true);
-      setIsLoading(false);
-    });
-  };
-
-  const toggleActive = () => {
-    let activeInactive = isActive
-      ? API_ENDPOINTS.MARK_INACTIVE
-      : API_ENDPOINTS.MARK_ACTIVE;
-    setIsLoading(true);
-    updateData(
-      `${API_ENDPOINTS.ADS}${API_ENDPOINTS.CARS}${activeInactive}/${data._id}`
-    ).then((response: any) => {
-      if (response && response.data && response.data.status === "success") {
-        setIsActive(!isActive);
-        setToastMessage(response.data.message);
-        setToastType("success");
-      } else {
-        console.log(response);
-        setToastMessage(response.message);
-        setToastType("error");
-      }
-      setToastOpen(true);
-      setIsLoading(false);
-    });
-  };
-
-  const deleteAd = () => {
-    setIsLoading(true)
-    deleteData(
-      `${API_ENDPOINTS.ADS}${API_ENDPOINTS.CARS}/${data._id}`
-    ).then((response: any) => {
-      if (response && response.data && response.data.status === "success") {
-        setToastMessage(response.data.message);
-        setToastType("success");
-      } else {
-        console.log(response);
-        setToastMessage(response.message);
-        setToastType("error");
-      }
-      setToastOpen(true);
-      setIsLoading(false);
-    });
-  };
+  const {
+    isActive,
+    toggleActive,
+    toggleBan,
+    deleteAd,
+    isBanned,
+    toastOpen,
+    setToastOpen,
+    toastType,
+    toastMessage,
+    isLoading,
+  } = useCarCard(data)
   return (
     <Card
       style={{ cursor: "pointer" }}
@@ -265,7 +209,7 @@ const CarCard = ({ data, layoutType = "list" }: CarCardProps) => {
                 <RemoveCircleOutline color="error" />
               )
             }
-            onClick={()=>toggleActive()}
+            onClick={() => toggleActive()}
             color="secondary"
             variant="contained"
             fullWidth
@@ -281,7 +225,7 @@ const CarCard = ({ data, layoutType = "list" }: CarCardProps) => {
                 <Block color="error" />
               )
             }
-            onClick={()=>toggleBan()}
+            onClick={() => toggleBan()}
             color="secondary"
             variant="contained"
             fullWidth
@@ -291,7 +235,7 @@ const CarCard = ({ data, layoutType = "list" }: CarCardProps) => {
 
           <Button
             endIcon={<Delete color="error" />}
-            onClick={()=>deleteAd()}
+            onClick={() => deleteAd()}
             title="Delete"
             color="secondary"
             variant="contained"
