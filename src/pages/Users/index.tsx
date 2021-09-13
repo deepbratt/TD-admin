a48f6a37083006b0ca46583792f5e751768004b0
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-09-13 15:28:41 +0500

    create user file and folder

---
+++ src/pages/Users/index.tsx
@@ -0,0 +1,12 @@
+import SecondaryLayout from "../../layout/SecondaryLayout"
+
+const Users = () =>{
+    return (
+        <SecondaryLayout>
+            
+        </SecondaryLayout>
+    )
+
+}
+
+export default Users
