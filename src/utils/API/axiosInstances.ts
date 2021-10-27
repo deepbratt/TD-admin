54f0811f4108de50311300056e7cc9689e4542df
Jomi Jose <jomivett@gmail.com>
2021-10-27 21:54:32 +0530

    fix

--- src/utils/API/axiosInstances.ts
+++ src/utils/API/axiosInstances.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://api.tezdealz.com/v1";
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
